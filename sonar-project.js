2dcf996d2afdacbc326e51ae60f37f21a6a0da70
mujeer <mujeer472@gmail.com>
2021-11-20 10:31:26 +0530

    updated details

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.54.107.143:9000/',
+  serverUrl: 'http://35.200.80.119:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectKey':'node-js',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': '08W31A0472',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
